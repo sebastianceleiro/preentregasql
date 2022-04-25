1ee944c1cf0d6f104f26f714ce3f34cee81627e9
Sebastián <87915563+sebastianceleiro@users.noreply.github.com>
2022-04-25 09:22:52 -0300

    Add files via upload

---
+++ database.js
@@ -0,0 +1,13 @@
+
+const options = {
+client: "mysql",
+connection: {
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "coder"
+}
+}
+
+export default options ;
